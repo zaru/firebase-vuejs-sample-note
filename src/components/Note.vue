1a5e8abd74fb4113a3b08f9fd95938332689726b
zaru <sakuraba@basicinc.jp>
2018-03-05 13:07:41 +0900

    added lodash

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -4,7 +4,7 @@
       {{ this.$route.params.noteId }}
     </div>
     <div class="main">
-      <textarea v-model="body" @keyup="sync"></textarea>
+      <textarea v-model="body" @keyup="sync()"></textarea>
     </div>
     <div class="right">
       <ul class="users">
@@ -17,26 +17,27 @@
 <script>
 import { mapGetters } from 'vuex'
 import {db} from '../firebase'
+import _ from 'lodash'
 
 export default {
   name: 'Note',
   data () {
     return {
+      note_id: null,
       body: '',
       users: []
     }
   },
   methods: {
-    sync () {
-      console.log(this.$route.params.noteId)
-      db.collection('notes').doc(this.$route.params.noteId).update({
+    sync: _.throttle(function () {
+      db.collection('notes').doc(this.note_id).update({
         body: this.body
       }).then(() => {
         console.log('Document successfully updated!')
       }).catch(error => {
         console.error('Error updating document: ', error)
       })
-    }
+    }, 2000)
   },
   beforeMount () {
     if (!this.$route.params.noteId) {
@@ -49,9 +50,20 @@ export default {
       }).catch(error => {
         console.error('Error adding document: ', error)
       })
+    } else {
+      this.note_id = this.$route.params.noteId
+      db.collection('notes').doc(this.note_id).get().then(doc => {
+        this.body = doc.data().body
+      }).catch(error => {
+        console.log(error)
+      })
     }
   },
   mounted () {
+    db.collection('notes').doc(this.$route.params.noteId)
+      .onSnapshot(doc => {
+        console.log('Current data: ', doc && doc.data())
+      })
   },
   computed: {
     ...mapGetters([
