c173435cdcda03b4ed988654689dd392e7e05d3b
zaru@sakuraba <zarutofu@gmail.com>
2018-03-09 00:34:24 +0900

    removed sign up parts

--- src/components/UserSession.vue
+++ src/components/UserSession.vue
@@ -1,21 +1,5 @@
 <template>
   <div class="user-session">
-    <h2>サインアップ</h2>
-    <dl>
-      <dt>Email</dt>
-      <dd><input type="text" v-model="email"></dd>
-      <dt>Password</dt>
-      <dd><input type="password" v-model="password"></dd>
-    </dl>
-    <button @click="signUp">submit</button>
-    <h2>サインイン</h2>
-    <dl>
-      <dt>Email</dt>
-      <dd><input type="text" v-model="email"></dd>
-      <dt>Password</dt>
-      <dd><input type="password" v-model="password"></dd>
-    </dl>
-    <button @click="signIn">submit</button>
   </div>
 </template>
 
@@ -26,44 +10,18 @@ export default {
   name: 'UserSession',
   data () {
     return {
-      user: null,
-      email: '',
-      password: ''
     }
   },
   methods: {
-    signUp () {
-      firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(
-        user => {
-          this.$router.push('/')
-        },
-        err => {
-          alert(err.message)
-        }
-      )
-    },
-    signIn () {
-      firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
-        user => {
-          this.$router.push('/')
-        },
-        err => {
-          alert(err.message)
-        }
-      )
-    }
   },
   beforeCreate () {
     firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        this.user = user
-      } else {
+      if (!user) {
         // 匿名ログイン
         firebase.auth().signInAnonymously().catch(console.error)
         firebase.auth().onAuthStateChanged(user => {
           if (user) {
             this.$router.push('/')
-          } else {
           }
         })
       }
